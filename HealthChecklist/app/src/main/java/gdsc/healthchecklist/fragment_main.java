0ac69139926d58c2a8511a3757c5983f5a40180b
Juminjae <45286407+Juminjae@users.noreply.github.com>
2022-02-28 23:58:57 +0900

    HealthChecklist_DevDay5
    BottomNavigationBar_prototype

---
+++ HealthChecklist/app/src/main/java/gdsc/healthchecklist/fragment_main.java
@@ -0,0 +1,20 @@
+package gdsc.healthchecklist;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_main extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle
+            savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page1_main, container, false);
+    }
+}
