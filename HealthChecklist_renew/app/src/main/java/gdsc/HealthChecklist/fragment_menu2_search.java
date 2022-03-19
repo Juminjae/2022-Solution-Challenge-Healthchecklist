a6247d9aa366f383ca5112d8228ca4d032d2b2d3
Juminjae <45286407+Juminjae@users.noreply.github.com>
2022-03-19 23:04:33 +0900

    HealthChecklist_DevDay9
    Button making and mapview setting

--- HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
+++ HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
@@ -10,10 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 public class fragment_menu2_search extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_fragment_menu2_search, container, false);
     }
+
 }
