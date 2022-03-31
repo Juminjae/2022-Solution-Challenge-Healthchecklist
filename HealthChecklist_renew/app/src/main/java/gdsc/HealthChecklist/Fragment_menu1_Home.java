074630ef0f0e20caf313ebc8c5ca4a631ef2dfcb
Juminjae <45286407+Juminjae@users.noreply.github.com>
2022-03-31 22:49:35 +0900

    HealthChecklist_DevDayFinal
    Text

--- HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/Fragment_menu1_Home.java
+++ HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/Fragment_menu1_Home.java
@@ -4,28 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Fragment_menu1_Home extends Fragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_fragment_menu1_home, container, false);
     }
-
-
-
-
 }
+
+
--- HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
+++ HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
@@ -17,30 +17,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class fragment_menu2_search extends Fragment
-        implements OnMapReadyCallback{
-    private MapView mapView;
-
-    public fragment_menu2_search(){
-
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-    }
+public class fragment_menu2_search extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_fragment_menu2_search, container, false);
 
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
     }
 }
