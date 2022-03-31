92d43d7fb82c92345fa27a8365226c300c7d1dd0
Juminjae <45286407+Juminjae@users.noreply.github.com>
2022-04-01 00:11:10 +0900

    HealthChecklist_DevDayFinish
    The End challenge
    (i'm gonna still uploading...)

--- HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
+++ HealthChecklist_renew/app/src/main/java/gdsc/HealthChecklist/fragment_menu2_search.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,14 +15,31 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class fragment_menu2_search extends Fragment {
+public class fragment_menu2_search extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mMap;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        oneMaker();
+    }
+
+    private void oneMaker() {
+
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_fragment_menu2_search, container, false);
-
     }
 }
